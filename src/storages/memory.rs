e9947ba312594c70550b19e43be41a14c18f2fb2
mperrot36 <159039684+mperrot36@users.noreply.github.com>
2025-02-05 15:10:58 +0100

    Add files via upload

--- src/storages/memory.rs
+++ src/storages/memory.rs
@@ -2,11 +2,64 @@ use std::collections::BTreeMap;
 
 use async_trait::async_trait;
 
-use crate::{domain::voting_machine::VotingMachine, storage::Storage};
+use crate::{domain::{generic_domains::{AttendenceSheet, Candidate, Score}, scoreboard::Scoreboard, voting_machine::VotingMachine}, storage::Storage};
 
 struct MemoryStore {
 	voting_machine: VotingMachine,
 }
 
 #[async_trait]
-impl Storage for MemoryStore {}
+impl Storage for MemoryStore {
+    
+    async fn new(machine: VotingMachine) -> Self {
+        MemoryStore {
+            voting_machine: machine,
+            
+        }
+    }
+
+   
+    async fn get_voting_machine(&self) -> VotingMachine {
+        self.voting_machine.clone() 
+    }
+
+
+    async fn put_voting_machine(&mut self, machine: VotingMachine) {
+        self.voting_machine = machine;
+    }
+}
+
+
+#[tokio::test]
+async fn my_test() {
+	let mut tableau_candidats = BTreeMap::new();
+
+	
+		
+		tableau_candidats.insert(Candidate("moi".to_string()), Score::default());
+	
+
+	let scoreboard = Scoreboard {
+		scores: tableau_candidats,
+		blank_score: Score::default(),
+		invalid_score: Score::default(),
+	};
+
+	let voters = AttendenceSheet::default();
+
+	let mut voting_machine = VotingMachine::new(voters, scoreboard);
+   
+    
+
+    let mut store = MemoryStore::new(voting_machine).await;
+    
+
+    let stored_machine = store.get_voting_machine().await;
+    
+
+    assert!(voting_machine==stored_machine);
+}
+
+
+
+
